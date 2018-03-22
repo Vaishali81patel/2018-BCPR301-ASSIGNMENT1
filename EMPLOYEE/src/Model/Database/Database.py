328b1600092471e6b0352b8a1d258a07c86910b9
Vaishali <vaishali81patel@gmail.com>
2018-03-22 14:47:00 +1300

    Changes applied

--- EMPLOYEE/src/Model/Database/Database.py
+++ EMPLOYEE/src/Model/Database/Database.py
@@ -19,7 +19,7 @@ class Database:
         # This function enable to handle database connection
         # Raise runtime error and type error if unable to connect database
         #
-        # Author: Vaishali Patel
+        # Author: Patel
         #
         try:
             self.db_conn = sqlite3.connect(db_conn)   # Try to connect the employee database
@@ -35,31 +35,35 @@ class Database:
         # This function is enable to create employee table
         # Raise runtime error and type error if unable to create employee table
         #
-        # Author: Vaishali Patel
+        # Author: Patel
         #
         # Create employee table if not exists
         try:
-            create_table = """ CREATE TABLE IF NOT EXISTS EMPLOYEE (employeeID VARCHAR (6), gender CHAR, age INTEGER,
-                               sales INTEGER, bmi VARCHAR(15), salary INTEGER, birthdate DATE) """
-        except ConnectionError:
-            print(ConnectionError)
-        except TypeError as error:
-            print (TypeError)
-        self.cursor.execute(create_table)
-        self.db_conn.commit()
+            create_table = """ 
+                            CREATE TABLE IF NOT EXISTS EMPLOYEE (employeeID VARCHAR (6), gender CHAR, age INTEGER,
+                            sales INTEGER, bmi VARCHAR(15), salary INTEGER, birthday DATE) 
+                            """
+            try:
+                self.cursor.execute (create_table)
+                self.db_conn.commit ()
+            except ConnectionError:
+                print(ConnectionError)
+            except TypeError as error:
+                print (TypeError)
+
 
     def insert_employee_data(self, data_arr):
         # This is function is enable to insert employee data into employee table
         # raise runtime error and type error if unable to insert employee data
         #
-        # Author: Vaishali Patel
+        # Author: Patel
         #
         #
         try:
             for employee in data_arr:   # Retrieve employee data in to data_arr "array" format
                 insert_string ="""
-                                INSERT INTO EMPLOYEE(employeeID, gender, age, sales, bmi, salary, birthdate)
-                                VALUES ("{employeeID}","{gender}","{age}","{sales}","{bmi}","{salary}","{birthdate}");                                    
+                                INSERT INTO EMPLOYEE(employeeID, gender, age, sales, bmi, salary, birthday)
+                                VALUES ("{employeeID}","{gender}","{age}","{sales}","{bmi}","{salary}","{birthday}");                                    
                                 """
             try:
                 insert_values = insert_string.format(employeeID = employee[0],
@@ -68,7 +72,7 @@ class Database:
                                                   sales = employee[3],
                                                   bmi = employee[4],
                                                   salary = employee[5],
-                                                  birthdate = employee[6])
+                                                  birthday = employee[6])
                 self.cursor.execute (insert_values)
                 self.db_conn.commit ()
             except ConnectionError:
@@ -87,5 +91,26 @@ class Database:
         #
         #
         dara_arr = [] # Retrieve employee data in dara_arr array format
-
-
+        try:
+            for employee in data_arr:  # Retrieve employee data in to data_arr "array" format
+                select_values="""
+                              SELECT * FROM EMPLOYEE
+                              """
+            try:
+                self.cursor.execute(select_values) # Execute the SQL Command
+                self.db.conn.commit()              # Commit the changes in Database
+                results = cursor.fetchall ()
+                for row in results:
+                    employeeID = raw[0],
+                    gender = raw[1],
+                    age = raw[2],
+                    sales = raw[3],
+                    bmi = raw[4],
+                    salary = raw[5],
+                    birthday = raw[6]
+            except ConnectionError:
+                print(ConnectionError)
+            except TypeError as error:
+                print (TypeError)
+            finally:
+                print(FileNotFoundError)
--- EMPLOYEE/src/Model/Database/Employee.py
+++ EMPLOYEE/src/Model/Database/Employee.py
@@ -3,12 +3,12 @@
 class Employee:
 
     # Initialize the attribute
-    def __init__(self, employeeID, gender, age, sales, bmi, salary, birthdate):
+    def __init__(self, employeeID, gender, age, sales, bmi, salary, birthday):
         self.employeeID = employeeID
         self.gender = gender
         self.age = age
         self.sales = sales
         self.bmi = bmi
         self.salary= salary
-        self.birthdate = birthdate
+        self.birthday = birthday
 

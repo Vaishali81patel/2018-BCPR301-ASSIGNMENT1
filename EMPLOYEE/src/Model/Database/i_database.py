cb3ea2595c15ab82b881133d8223898a95b5e829
Vaishali <vaishali81patel@gmail.com>
2018-03-29 12:43:42 +1300

    Done

--- EMPLOYEE/src/Model/Database/i_database.py
+++ EMPLOYEE/src/Model/Database/i_database.py
@@ -1,9 +1,11 @@
 # Create exceptional error handling interface class
 # This is an interface class of Database base class
+
+
 class IDatabase(object):
 
     @property
-    def create_database_connection(self, database_name='my_db'):
+    def create_database_connection(self, database_name='mydb'):
         raise NotImplementedError("An Abstract method has not been implemented yet!")
 
     def insert_employee_data(self, employee_ob):

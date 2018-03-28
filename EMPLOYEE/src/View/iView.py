6b17043d8ddf8c33685d19c75313a55c637db378
Vaishali <vaishali81patel@gmail.com>
2018-03-29 01:08:33 +1300

    PEP8 Code Check Compeleted

--- EMPLOYEE/src/View/iView.py
+++ EMPLOYEE/src/View/iView.py
@@ -9,12 +9,11 @@ class EmployeeData_View:
     #
     # Written By: Patel
     #
-
     @staticmethod
     def display(display_string):
         # This function enable to display a given string to the screen.
         #
-        print (display_string)
+        print(display_string)
 
     @staticmethod
     def read(prompt):
@@ -24,28 +23,33 @@ class EmployeeData_View:
         #
         # Written: Patel
         #
-        return input (prompt)
-
+        return input(prompt)
 
     def manual_employee_data_entry(self):
-        # This function is enable user to enter employee's data manually
+        # This function is enable user to \
+        # enter employee's data manually
         # Hereby; I have made the employee_data_arr "array"
-        # Which will be keep appending as the user put the data over and over again
+        # Which will be keep appending as \
+        # the user put the data over and over again \
         #
-        # Written ByL Patel
+        # Written By: Patel
         #
         emp_data_arr = []
-        employee_data = [self.read ("Enter Employee ID: "), self.read ("Enter gender: "),
-                       self.read ("Enter age: "), self.read ("Enter sales count: "),
-                       self.read ("Enter BMI: "), self.read ("Enter salary: "),
-                       self.read ("Enter birthday, e.g. dd-mm-yyyy: ")]
-        emp_data_arr.append (employee_data)
+        employee_data = [
+                        self.read("Enter Employee ID: "),
+                        self.read("Enter gender: "),
+                        self.read("Enter age: "),
+                        self.read("Enter sales count: "),
+                        self.read("Enter BMI: "), self.read("Enter salary: "),
+                        self.read("Enter birthday, e.g. dd-mm-yyyy: ")
+                        ]
+        emp_data_arr.append(employee_data)
         return emp_data_arr
-
 
     @staticmethod
     def display_graph(graph_data):
-        # This function is enable to take a graph object from the graph generator function
+        # This function is enable to take a graph \
+        # object from the graph generator function
         # and display it in the web browser
         #
         # Written By: Patel
@@ -61,19 +65,22 @@ class EmployeeData_View:
         emp_gender_data = []
         emp_sales_data = []
         for employee in emp_data_arr:
-            emp_gender_data.append (employee[1])
-            sales = int (employee[3])
-            emp_sales_data.append (sales)
-        graph_data = [ob.Bar (x=emp_gender_data, y=emp_sales_data)]
-        graph_format = ob.Layout (title="Gender Verse Sales Bar Graph", xaxis=dict (title="Gender"),
-            yaxis=dict (title="Sales"))
-        graph = ob.Figure (data=graph_data, layout=graph_format)
+            emp_gender_data.append(employee[1])
+            sales = int(employee[3])
+            emp_sales_data.append(sales)
+        graph_data = [ob.Bar(x=emp_gender_data, y=emp_sales_data)]
+        graph_format = ob.Layout(
+            title="Gender Verse Sales Bar Graph",
+            xaxis=dict(title="Gender"),
+            yaxis=dict(title="Sales")
+            )
+        graph = ob.Figure(data=graph_data, layout=graph_format)
         self.display_graph(graph)
-
 
     def employees_by_gender_graph(self, emp_data_arr):
         # This function is enable to display employee
-        # Male and female ratio based on the employee data stored in the system
+        # Male and female ratio based on the
+        # employee data stored in the system
         #
         # Written By: Patel
         #
@@ -85,36 +92,40 @@ class EmployeeData_View:
             if employee[1] == "F":
                 emp_female_count += 1
 
-        graph_data = {'data': [{'labels': ['Male', 'Female'], 'values':
-                                [emp_male_count, emp_female_count], 'type': 'pie'}],
+        graph_data = {'data': [
+                                {'labels': ['Male', 'Female'], 'values':
+                                [emp_male_count, emp_female_count],
+                                'type': 'pie'}],
             'layout': {'title': 'Number of Employees by Gender'}}
         self.display_graph(graph_data)
 
-
     def age_verse_salary_graph(self, emp_data_arr):
-        # This function is enable to display Scatter graph of employee
-        # Male vs Female and their salary based on the data stored in the system
-        # The function calculate the actual salary by deviede the salary by 1000
-        # Then pass that information to the display graph function
+        # This function is enable to display \
+        # Scatter graph of employee
+        # Male vs Female and their salary based on \
+        # the data stored in the system
+        # The function calculate the actual salary \
+        # by deviede the salary by 1000
+        # Then pass that information to \
+        # the display graph function
         #
         # Written By: Patel
         #
         emp_age_data = []
         emp_salary_data = []
         for employee in emp_data_arr:
-            emp_age_data.append (employee[2])
-            salary = int (employee[5]) * 1000
-            salary_data.append (salary)
+            emp_age_data.append(employee[2])
+            salary = int(employee[5]) * 1000
+            salary_data.append(salary)
         graph_data = [
             ob.Scatter (x=emp_age_data, y=salary_data,
-                        marker=dict (color="rgb(16, 32, 77)"),
+                        marker=dict(color="rgb(16, 32, 77)"),
                         name="Age Verse Salary")]
-        graph_format = ob.Layout (title="Salary Verse Age Scatter Graph",
-                                  xaxis=dict (title="Age"),
-            yaxis=dict (title="Salary"))
-        graph = ob.Figure (data=graph_data, layout=graph_format)
+        graph_format = ob.Layout(title="Salary Verse Age Scatter Graph",
+                                  xaxis=dict(title="Age"),
+            yaxis=dict(title="Salary"))
+        graph =ob.Figure(data=graph_data, layout=graph_format)
         self.display_graph(graph)
-
 
     def bmi_pie_graph(self, emp_data_arr):
         # Thie function is enable to display pie graphof the employee
@@ -136,7 +147,11 @@ class EmployeeData_View:
             if employee[4] == "Underweight":
                 emp_underweight_count += 1
         graph_data = {'data': [{'labels': ['Normal', 'Overweight', 'Obesity', 'Underweight'],
-                                'values': [emp_normal_count, emp_overweight_count,
-                                           emp_obesity_count, emp_underweight_count],
-                                'type': 'pie'}], 'layout': {'title': 'Staff by BMI'}}
+                                'values': [
+                                    emp_normal_count,
+                                    emp_overweight_count,
+                                    emp_obesity_count,
+                                    emp_underweight_count
+                                    ], 'type': 'pie'}],
+                                    'layout': {'title': 'Staff by BMI'}}
         self.display_graph(graph_data)

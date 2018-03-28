b10d2838a9498d3265096ffa92a88b2dd9827be2
Vaishali <vaishali81patel@gmail.com>
2018-03-28 23:38:29 +1300

    PEP8 Checks Done
    Completed

--- EMPLOYEE/src/Main.py
+++ EMPLOYEE/src/Main.py
@@ -6,16 +6,13 @@ from DataValidation.EmpDataValidator import *
 from Model.Database.Database import *
 import sys
 
-
 if __name__ == '__main__':
     InterpreterController(EmployeeData_View(), Interpreter(
-                            FileHandler(),
-                            DataValidator(),
-                            Database (),
-                            InterpreterController.set_file_path(sys.argv)
-                            )
-                        ).cmdloop()
+        FileHandler(), DataValidator(), Database(),
+        InterpreterController.set_file_path(sys.argv))).cmdloop()
 
-                            # sys.argv is a list in Python, which contains the command-line
-                            # arguments passed to the script.
-                            # With the len(sys.argv) function you can count the number of arguments.
+# sys.argv is a list in Python,
+# which contains the command-line
+# arguments passed to the script.
+# With the len(sys.argv) function
+# you can count the number of arguments.
